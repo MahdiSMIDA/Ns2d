00dde5e9a87f7a7794a87fe44d03519a6220ec4c
MahdiSMIDA <eng.smida@gmail.com>
2017-12-22 09:50:21 +0100

    Update module_mesh.f90

--- module_mesh.f90
+++ module_mesh.f90
@@ -3,6 +3,7 @@ module module_mesh
 
   type tmesh
      integer :: nx,ny
+     real(kind=8) ::hx,hy,xmin,xmax,ymin,ymax
      real(kind=8) ,dimension(:),allocatable :: xc,xf
      real(kind=8) ,dimension(:),allocatable :: yc,yf
   end type tmesh
@@ -13,8 +14,61 @@ contains
     type(tmesh)  :: self
     integer      :: nx,ny
     real(kind=8) :: xmin,xmax,ymin,ymax
+    print*,"tmesh_allocate"
+
+    self%nx=nx
+    self%ny=ny
+    allocate(self%xc(0:nx+1)); self%xc=0.0
+    allocate(self%yc(0:ny+1))
+    allocate(self%xf(0:nx+1))
+    allocate(self%yf(0:ny+1))
+
+
+    hx=(xmax-xmin)/nx
+    hy=(ymax-ymin)/ny
+
     
-  end subroutine tmesh_allocate
+    
+    do  i=0,nx+1
+       self%xc(i)= (i-0.5)*hx 
+    end do
+    
+    do i=0,nx+1
+       self%xf(i)=i*hx
+    end do
+    
+    do j=0,ny+1
+       self%yc(j)= (j-0.5)*hy 
+    end do
+    do j=0,ny+1
+       self%yf(j)=j*hy
+    end do
+    
+    self%hy=hy
+    self%hx=hx
+    self%xmin=xmin
+    self%ymax=ymax
+    self%xmax=xmax
+    self%ymax=ymax
+    
+    
+
+    print*,"x"
+    
+    do i=0,nx+1
+       print*,i,self%xc(i),self%xf(i)
+    end do
+    print*,"y"
+    
+    do j=0,ny+1
+       print*,j,self%yc(j),self%yf(j)
+    end do
+    
+    
+end subroutine tmesh_allocate
+
+
+
 
   subroutine tmesh_allocate_var(self,dg01,dg02,dg03)
     type(tmesh)  :: self
